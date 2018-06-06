52a660d70107a8f49bf40daa5efb8ea73edb4ad6
Cody <33329190+Cody47116@users.noreply.github.com>
2018-06-06 16:08:47 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const log     = require('./tools/logger')
 const request = require('request')
 const fs      = require('fs')
 
-log('---------------------------')
-log("-----Made by @Cody_Ncc-----")
-log('---------------------------')
-log('')
 
 function main() {
-    let config = JSON.parse(fs.readFileSync('config.json', 'utf-8'))
+    let config  = JSON.parse(fs.readFileSync('config.json', 'utf-8'))
     let session = request.jar()
-    let i = 0
+    let i       = 0
 
     captcha({
         'apikey': config.capKey,
@@ -34,9 +30,65 @@ function main() {
         }
         request(login, (err, res, body) => {
             if (res.statusCode == 200) {
-                log('Login Success!', 'success')
                 let token = JSON.stringify(body).split(',')[0].split(':')[1].replace('"', '').slice(0, 172)
+                log('Login Success!', 'success')
+
+                if (i < parseInt(config.amount))
+                    setTimeout(create, 500)
+                else {
+                    request({
+                        url: 'https://privacy.com/api/v1/card',
+                        jar: session,
+                        method: 'get',
+                        headers: {
+                            'Authorization': `Bearer ${token}`,
+                            'Origin': 'https://privacy.com',
+                            'Referer': 'https://privacy.com/home',
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
+                        }
+                    }, (err, res, body) => {
+                        let cards    = JSON.parse(body)
+                        let profiles = []
+
+                        for (let i = 0; i < cards.cardList.length; i++) {
+                            if (cards.cardList[i].state == "OPEN") {
+                                profiles.push({
+                                    "Billing"      : {
+                                        "Address"  : config.address,
+                                        "Apt"      : config.apt,
+                                        "City"     : config.city,
+                                        "FirstName": config.firstName,
+                                        "LastName" : config.lastName,
+                                        "State"    : config.state,
+                                        "Zip"      : config.zip
+                                    },
+                                    "CCNumber"     : cards.cardList[i].PAN,
+                                    "CVV"          : cards.cardList[i].CVV,
+                                    "CardType"     : "Visa",
+                                    "Country"      : config.country,
+                                    "ExpMonth"     : cards.cardList[i].expMonth,
+                                    "ExpYear"      : cards.cardList[i].expYear,
+                                    "Name"         : cards.cardList[i].memo,
+                                    "Phone"        : config.phone,
+                                    "Same"         : true,
+                                    "Shipping"     : {
+                                        "Address"  : config.address,
+                                        "Apt"      : config.apt,
+                                        "City"     : config.city,
+                                        "FirstName": config.firstName,
+                                        "LastName" : config.lastName,
+                                        "State"    : config.state,
+                                        "Zip"      : config.zip
+                                    }
+                                })
+                            }
+                        }
+                        fs.writeFileSync('profiles.json', JSON.stringify(profiles))
+                        log('Exported Cards to profiles.json.', 'success')
+                    })
+                }
                 function create() {
+
                     let createOpts = {
                         url: 'https://privacy.com/api/v1/card',
                         jar: session,
@@ -59,35 +111,14 @@ function main() {
                         }
                     }
                     request(createOpts, (err, res, opts) => {
-                        if (res !== undefined) {
+                        if (res !== undefined && res.statusCode == 200)
                             log(`Finished creating card ${config.cardName} ${i}`, 'success')
-                            if (i < parseInt(config.amount))
-                                setTimeout(create, 500)
-                            else {
-                                request({
-                                    url: 'https://privacy.com/api/v1/card',
-                                    jar: session,
-                                    method: 'get',
-                                    headers: {
-                                        'Authorization': `Bearer ${token}`,
-                                        'Origin': 'https://privacy.com',
-                                        'Referer': 'https://privacy.com/home',
-                                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
-                                    }
-                                }, (err, res, body) => {
-                                    fs.writeFileSync('cards.json', body, (err) => {
-                                        log('Exported Cards to cards.json', 'success')
-                                    })
-                                })
-                            }
-                        }
                         i++
                     })
                 }
-                create();
             }
             else
-                log('Login Failed...', 'error')
+                log('Login Failed. Try Again.', 'error')
         })
     })
 }
